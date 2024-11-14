e6ce79db54a2acec43629ccd6f16c478b1d2da3a
Przemek Sz <przemek@mac.home>
2024-11-14 22:40:44 +0100

    updated database User model structure

--- server/src/db/models/User.ts
+++ server/src/db/models/User.ts
@@ -1,4 +1,4 @@
-import { Schema, Document } from 'mongoose';
+import { Schema, Document, type InferSchemaType } from 'mongoose';
 import mongoose from '../connection.ts';
 
 interface IUser extends Document {
@@ -9,11 +9,12 @@ interface IUser extends Document {
   storageLimit: number;
   usedStorage: number;
   maxUploadSize: number;
+  verifiedAt: Date | null;
   createdAt: Date;
   updatedAt: Date;
 }
 
-const UserSchema: Schema = new Schema({
+const UserSchema: Schema<IUser> = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -21,9 +22,13 @@ const UserSchema: Schema = new Schema({
   storageLimit: { type: Number, required: true, default: 0 },
   usedStorage: { type: Number, required: true, default: 0 },
   maxUploadSize: { type: Number, required: true, default: 0 },
+  verifiedAt: { type: Date, default: null },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model<IUser>('User', UserSchema);
+
+type UserType = InferSchemaType<typeof UserSchema>;
+export type { UserType };
 export default User;

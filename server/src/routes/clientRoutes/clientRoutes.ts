35f784db2ce29fe478614624279454e5c4a80fc8
modek4 <modek4@onet.com.pl>
2024-11-19 20:31:56 +0100

    cloud.ejs

--- server/src/routes/clientRoutes/clientRoutes.ts
+++ server/src/routes/clientRoutes/clientRoutes.ts
@@ -9,7 +9,41 @@ clientRoutes.get('/', (req, res) => {
 });
 
 clientRoutes.get('/cloud', auth(), (req, res) => {
-  res.render('cloud.ejs');
+  const user = {
+    username: req.user?.username || 'Guest',
+    email: req.user?.email || null,
+    role: req.user?.role || process.env.ROLE_GUEST
+  }
+
+  // TODO get files from database
+  const files: {
+    id: number,
+    name: string,
+    path: string,
+    size: string,
+    date: string,
+    type: string
+  }[] = [{
+    id: 1, //? _id from database
+    name: 'file1',
+    path: '/path/to/file1.pdf',
+    size: '1MB',
+    date: '2021-03-01', //? Last modified date
+    type: 'pdf'
+  }, {
+    id: 2,
+    name: 'dir1',
+    path: '/path/to/dir1',
+    size: '0',
+    date: '2021-03-01',
+    type: 'dir'
+  }];
+
+
+  res.render('cloud.ejs', {
+    user: user,
+    files: files || []
+  });
 });
 
 clientRoutes.get('/not-verified', (req, res) => {
